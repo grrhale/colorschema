41be313b1701a3a7eff8a6e8d1045ae2788e1a11
rhale <raymond.hale0@gmail.com>
2023-02-07 19:13:23 -0500

    now takes image filepath from user

--- colorschema.py
+++ colorschema.py
@@ -1,39 +1,45 @@
 # lily.jpg was 720px * 540px originally
 
-#import imageio
 import imageio.v2 as imageio
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.cluster import KMeans
-import cv2
-from skimage.color import rgb2lab, deltaE_cie76
-from collections import Counter
-import os
+import os.path
 
-pic=imageio.imread('/home/rhale/git/colorschema/lily.jpg')
+# receive file from user
+print("Please provide the filepath of an image to analyze:")
+filepath = input()
+file_exists = os.path.exists(filepath)
 
-print('Type of the image : ',type(pic))
-print('Shape of the image : {}'.format(pic.shape))
-print('Height of the image {}'.format(pic.shape[0]))
-print('Width of the image {}'.format(pic.shape[1]))
-print('Dimension of the Image {}\n'.format(pic.ndim)) 
+if file_exists:
+	imageio.imread(filepath)
+	
+	pic=imageio.imread(filepath)
 
-print('Image size {}'.format(pic.size))
-print('Maximum RGB value in this image {}'.format(pic.max()))
-print('Minimum RGB value in this image {}\n'.format(pic.min())) 
+	print('Type of the image : ',type(pic))
+	print('Shape of the image : {}'.format(pic.shape))
+	print('Height of the image {}'.format(pic.shape[0]))
+	print('Width of the image {}'.format(pic.shape[1]))
+	print('Dimension of the image {}\n'.format(pic.ndim)) 
 
-print('Value of only R channel {}'.format(pic[ 150, 100, 0]))
-print('Value of only G channel {}'.format(pic[ 150, 100, 1]))
-print('Value of only B channel {}'.format(pic[ 150, 100, 2])) 
+	print('Image size {}'.format(pic.size))
+	print('Maximum RGB value in this image {}'.format(pic.max()))
+	print('Minimum RGB value in this image {}\n'.format(pic.min())) 
 
-plt.title('R channel')
-plt.imshow(pic[ : , : , 0])
-plt.show()
+	print('Value of only R channel {}'.format(pic[ 150, 100, 0]))
+	print('Value of only G channel {}'.format(pic[ 150, 100, 1]))
+	print('Value of only B channel {}'.format(pic[ 150, 100, 2])) 
 
-plt.title('G channel')
-plt.imshow(pic[ : , : , 1])
-plt.show() 
+	plt.title('R channel')
+	plt.imshow(pic[ : , : , 0])
+	plt.show()
 
-plt.title('B channel')
-plt.imshow(pic[ : , : , 2])
-plt.show() 
+	plt.title('G channel')
+	plt.imshow(pic[ : , : , 1])
+	plt.show() 
+
+	plt.title('B channel')
+	plt.imshow(pic[ : , : , 2])
+	plt.show()
+
+else: 
+	print("File does not exist.")
