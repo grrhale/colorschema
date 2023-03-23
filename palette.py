98cdd430a9abbf9d4d91d44b5a36ba6eb55375ee
rhale <raymond.hale0@gmail.com>
2023-03-22 20:51:14 -0400

    hexadecimal color values of each pixel now available

--- palette.py
+++ palette.py
@@ -3,6 +3,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os.path
 import pandas as pd
+from itertools import product
+
+# function to convert rgb values to hexadecimal values
+def rgb2hex(red, green, blue):
+	return '#%02x%02x%02x' % (red, green, blue)
 
 # ask user for file
 print("Please provide the filepath of an image to analyze:")
@@ -14,47 +19,36 @@ file_exists = os.path.exists(filepath)
  then print information about the image. after, plot the R G B channels.
 """
 #figure, plots = plt.subplots(ncols=3, nrows=1)
+i = 0
 
 if file_exists:
-	# read user image into numpy ndarray
-	pic = cv2.imread(filepath)
-	# convert image to grayscale for np.where
-	gray = cv2.cvtColor(pic,cv2.COLOR_BGR2GRAY)
-	# create array of xy coordinates of each pixel in the image using np.where
-	xy_coords = np.flip(np.column_stack(np.where(gray >= 0)), axis=1)
 	
-	for i in xy_coords:
-		x=(i[0])
-		y=(i[1])
-		red_values = format(pic[x, y, 2])
-		green_values = format(pic[x, y, 1])
-		blue_values = format(pic[x, y, 0])
-		
-		pixel_values = (red_values, green_values, blue_values)
-		print(pixel_values)
-# print information about the file
-#	print('Type of the image: ',type(pic))
-#	print('Shape of the image: {}'.format(pic.shape))
-#	print('Height of the image: {}'.format(pic.shape[0]))
-#	print('Width of the image: {}'.format(pic.shape[1]))
-#	print('Dimension of the image: {}\n'.format(pic.ndim)) 
+	# read user image into numpy ndarray with cv2
+	pic = cv2.imread(filepath)
+	# create list of coordinates of each pixel in the image
+	coordinates = list(product(range(pic.shape[0]), range(pic.shape[1])))
+	
+	# declare rgb lists
+	rl=[]
+	gl=[]
+	bl=[]
+	
+	# loop to extract rgb values from each pixel and add them to lists
+	while i < len(coordinates):
+		rl.append(format(pic[coordinates[i][0], coordinates[i][1], 2]))
+		gl.append(format(pic[coordinates[i][0], coordinates[i][1], 1]))
+		bl.append(format(pic[coordinates[i][0], coordinates[i][1], 0]))
+		i += 1
+	# combine lists into array of rgb values
+	rgb_vals = np.array([rl, gl, bl])
+	# create dataframe of values, rows = r,g,b, one column per pixel
+	df_rgb = pd.DataFrame(rgb_vals)
+	
+	for i in range(pic.size):
+		print(rgb2hex(int(df_rgb.iloc[0, i]), int(df_rgb.iloc[1, i]), int(df_rgb.iloc[2, i])))
 
-#	print('Image size {}'.format(pic.size))
-#	print('Maximum RGB value in this image {}'.format(pic.max()))
-#	print('Minimum RGB value in this image {}\n'.format(pic.min())) 
-
-#	print('Value of only R channel {}'.format(pic[100, 150, 2]))
-#	print('Value of only G channel {}'.format(pic[100, 150, 1]))
-#	print('Value of only B channel {}'.format(pic[100, 150, 0])) 
-
-# plot the red green and blue channels
-#	for i, subplot in zip(range(3), plots):
-#		temp = np.zeros(pic.shape, dtype='uint8')
-#		temp[:,:,i] = pic[:,:,i]
-#		subplot.imshow(temp)
-#		subplot.set_axis_off()
-#	plt.show()
-
+	
+	
 # if the image filepath provided doesn't point to an image, end with msg	
 else: 
 	print("File not found.")
