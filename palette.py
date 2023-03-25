b5b12135c7cc081f2ceaa14fdd5e18885ea6bbf8
rhale <raymond.hale0@gmail.com>
2023-03-24 20:36:14 -0400

    DataFrame accurately contains hex values

--- palette.py
+++ palette.py
@@ -10,17 +10,21 @@ def rgb2hex(red, green, blue):
 	return '#%02x%02x%02x' % (red, green, blue)
 
 # ask user for file
-print("Please provide the filepath of an image to analyze:")
-filepath = input()
+filepath = input(f"Please provide the filepath of an image to analyze: ")
 file_exists = os.path.exists(filepath)
 
-"""
- read file given by user if it exists,
- then print information about the image. after, plot the R G B channels.
-"""
 #figure, plots = plt.subplots(ncols=3, nrows=1)
-i = 0
 
+# declare rgb lists
+r_vals=[]
+g_vals=[]
+b_vals=[]
+# declare hexadecimal lists
+hex_code = []
+hex_list=[]
+# declare i variable for loops
+i = 0
+	
 if file_exists:
 	
 	# read user image into numpy ndarray with cv2
@@ -28,24 +32,28 @@ if file_exists:
 	# create list of coordinates of each pixel in the image
 	coordinates = list(product(range(pic.shape[0]), range(pic.shape[1])))
 	
-	# declare rgb lists
-	rl=[]
-	gl=[]
-	bl=[]
-	
-	# loop to extract rgb values from each pixel and add them to lists
+	# loop to extract rgb values from each pixel by coordinate and add them to lists
 	while i < len(coordinates):
-		rl.append(format(pic[coordinates[i][0], coordinates[i][1], 2]))
-		gl.append(format(pic[coordinates[i][0], coordinates[i][1], 1]))
-		bl.append(format(pic[coordinates[i][0], coordinates[i][1], 0]))
+		r_vals.append(format(pic[coordinates[i][0], coordinates[i][1], 2]))
+		g_vals.append(format(pic[coordinates[i][0], coordinates[i][1], 1]))
+		b_vals.append(format(pic[coordinates[i][0], coordinates[i][1], 0]))
 		i += 1
 	# combine lists into array of rgb values
-	rgb_vals = np.array([rl, gl, bl])
+	rgb_vals = np.array([r_vals, g_vals, b_vals])
 	# create dataframe of values, rows = r,g,b, one column per pixel
 	df_rgb = pd.DataFrame(rgb_vals)
+	print('Analyzing...')
 	
-	for i in range(pic.size):
-		print(rgb2hex(int(df_rgb.iloc[0, i]), int(df_rgb.iloc[1, i]), int(df_rgb.iloc[2, i])))
+	for i in range(len(coordinates)):
+		hex_code = [rgb2hex(
+		int(df_rgb.iloc[0, i]), 
+		int(df_rgb.iloc[1, i]), 
+		int(df_rgb.iloc[2, i]))]
+		hex_list.append(hex_code)
+
+	df_hex = pd.DataFrame(hex_list)
+	print(df_hex)
+		
 
 	
 	
