e88017fff4056f7ab7e0df1141b9d317aa37fbd1
rhale <raymond.hale0@gmail.com>
2023-03-26 11:38:59 -0400

    created function extract_rgb() to refactor, prepare for plot code

--- color_module.py
+++ color_module.py
@@ -1,3 +1,23 @@
-# convert rgb values to hexadecimal values
+import cv2
+from itertools import product
+
+# extract_rgb() returns dictionary of rgb values in image, given ndarray of rgb values
+def extract_rgb(image_as_ndarray):
+	i = 0
+	r_vals=[]
+	g_vals=[]
+	b_vals=[]
+	coordinates = list(product(range(image_as_ndarray.shape[0]), 
+	range(image_as_ndarray.shape[1])))
+	
+	while i < len(coordinates):
+		r_vals.append(format(image_as_ndarray[coordinates[i][0], coordinates[i][1], 2]))
+		g_vals.append(format(image_as_ndarray[coordinates[i][0], coordinates[i][1], 1]))
+		b_vals.append(format(image_as_ndarray[coordinates[i][0], coordinates[i][1], 0]))
+		i += 1
+	rgb_data = {'R': r_vals, 'G': g_vals, 'B': b_vals}	
+	return rgb_data
+	
+# rgb2hex() converts rgb values to hexadecimal values, given RGB input
 def rgb2hex(red, green, blue):
 	return '#%02x%02x%02x' % (red, green, blue)
--- palette.py
+++ palette.py
@@ -3,48 +3,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os.path
 import pandas as pd
-from itertools import product
 import color_module as cm
 
 # ask user for file
 filepath = input(f"Please provide the filepath of an image to analyze: ")
 file_exists = os.path.exists(filepath)
 
-# declare rgb lists
-r_vals=[]
-g_vals=[]
-b_vals=[]
-
-# declare i variable for loops
-i = 0
-
 if file_exists:
-	
 	# read user image into numpy ndarray with cv2
-	pic = cv2.imread(filepath)
-	# create list of coordinates of each pixel in the image
-	coordinates = list(product(range(pic.shape[0]), range(pic.shape[1])))
-	
-	# loop to extract rgb values from each pixel by coordinate and add them to lists
-	while i < len(coordinates):
-		r_vals.append(format(pic[coordinates[i][0], coordinates[i][1], 2]))
-		g_vals.append(format(pic[coordinates[i][0], coordinates[i][1], 1]))
-		b_vals.append(format(pic[coordinates[i][0], coordinates[i][1], 0]))
-		i += 1
-	# combine lists into dictionary of rgb values
-	rgb_data = {'R': r_vals, 'G': g_vals, 'B': b_vals}
+	pic = cv2.imread(filepath)	
 	# create dataframe of rgb values
-	df_rgb = pd.DataFrame(rgb_data)
-
-	# convert rgb values to hexadecimal, and append them to the dataframe (as new df, df_hex) 
+	df_rgb = pd.DataFrame(cm.extract_rgb(pic))
+	# create df_hex from df_rgb, adding hex values and stripping RGB vals 
 	df_hex = df_rgb.assign(
 	hex=df_rgb.astype(int).apply(lambda r: cm.rgb2hex(*r), axis=1))
+	
 	# eliminate hex values that only appear once from the dataframe
 	df_hex = df_hex[df_hex.duplicated(['hex'])]
 	# eliminate the R G B values from the dataframe, leaving only recurring hexes
 	df_hex = df_hex.drop(['R', 'G', 'B'], axis=1)
-	# temporary prints, to check results
-	df_hex.to_csv('lily.csv')
+	# temporary print, to check results
 	print(df_hex)
 
 # if the image filepath provided didn't point to an image, end with msg	
