6a5b160c4f5a1f944ae4114018c691d2bb41948c
rhale <raymond.hale0@gmail.com>
2023-03-25 21:46:28 -0400

    updated rgb list to dict, now appends hex to rgb dataframe

--- palette.py
+++ palette.py
@@ -4,27 +4,20 @@ import numpy as np
 import os.path
 import pandas as pd
 from itertools import product
-
-# function to convert rgb values to hexadecimal values
-def rgb2hex(red, green, blue):
-	return '#%02x%02x%02x' % (red, green, blue)
+import color_module as cm
 
 # ask user for file
 filepath = input(f"Please provide the filepath of an image to analyze: ")
 file_exists = os.path.exists(filepath)
 
-#figure, plots = plt.subplots(ncols=3, nrows=1)
-
 # declare rgb lists
 r_vals=[]
 g_vals=[]
 b_vals=[]
-# declare hexadecimal lists
-hex_code = []
-hex_list=[]
+
 # declare i variable for loops
 i = 0
-	
+
 if file_exists:
 	
 	# read user image into numpy ndarray with cv2
@@ -38,25 +31,16 @@ if file_exists:
 		g_vals.append(format(pic[coordinates[i][0], coordinates[i][1], 1]))
 		b_vals.append(format(pic[coordinates[i][0], coordinates[i][1], 0]))
 		i += 1
-	# combine lists into array of rgb values
-	rgb_vals = np.array([r_vals, g_vals, b_vals])
-	# create dataframe of values, rows = r,g,b, one column per pixel
-	df_rgb = pd.DataFrame(rgb_vals)
-	print('Analyzing...')
-	
-	for i in range(len(coordinates)):
-		hex_code = [rgb2hex(
-		int(df_rgb.iloc[0, i]), 
-		int(df_rgb.iloc[1, i]), 
-		int(df_rgb.iloc[2, i]))]
-		hex_list.append(hex_code)
+	# combine lists into dictionary of rgb values
+	rgb_data = {'R': r_vals, 'G': g_vals, 'B': b_vals}
+	# create dataframe of rgb values
+	df_rgb = pd.DataFrame(rgb_data)
 
-	df_hex = pd.DataFrame(hex_list)
+	# convert rgb values to hexadecimal, and append them to the dataframe
+	df_hex = df_rgb.assign(
+	hex=df_rgb.astype(int).apply(lambda r: cm.rgb2hex(*r), axis=1))
 	print(df_hex)
-		
 
-	
-	
-# if the image filepath provided doesn't point to an image, end with msg	
+# if the image filepath provided didn't point to an image, end with msg	
 else: 
 	print("File not found.")
